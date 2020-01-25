cfe663a796e057c062e3245ce8c74b17ea4de033
j-cleary914 <j.cleary914@gmail.com>
2020-01-25 20:44:06 +0000

    will update position correctly for a single instruction

--- rover.js
+++ rover.js
@@ -4,6 +4,42 @@ let rover = {
     console.log({ initialPos }, { instructions });
     this.pos = initialPos;
 
+    for (let i = 0; i < instructions.length; i++) {
+      console.log(instructions[0]);
+    }
+
+    if (instructions === "R") {
+      if (this.pos[2] === "N") {
+        this.pos[2] = "E";
+      } else if (this.pos[2] === "E") {
+        this.pos[2] = "S";
+      } else if (this.pos[2] === "S") {
+        this.pos[2] = "W";
+      } else {
+        this.pos[2] = "N";
+      }
+    } else if (instructions === "L") {
+      if (this.pos[2] === "N") {
+        this.pos[2] = "W";
+      } else if (this.pos[2] === "E") {
+        this.pos[2] = "N";
+      } else if (this.pos[2] === "S") {
+        this.pos[2] = "E";
+      } else {
+        this.pos[2] = "S";
+      }
+    } else if (instructions === "M") {
+      if (this.pos[2] === "N") {
+        this.pos[1]++;
+      } else if (this.pos[2] === "E") {
+        this.pos[0]++;
+      } else if (this.pos[2] === "S") {
+        this.pos[1]--;
+      } else {
+        this.pos[0]--;
+      }
+    }
+
     return this.pos;
   }
 };
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -7,4 +7,23 @@ describe("updatePos", () => {
     expect(updatePos([2, 4, "E"], "")).to.eql([2, 4, "E"]);
     expect(updatePos([4, 4, "S"], "")).to.eql([4, 4, "S"]);
   });
+
+  it("updates bearing correctly when given single instruction to turn right", () => {
+    expect(updatePos([1, 1, "N"], "R")).to.eql([1, 1, "E"]);
+    expect(updatePos([1, 1, "E"], "R")).to.eql([1, 1, "S"]);
+    expect(updatePos([1, 1, "S"], "R")).to.eql([1, 1, "W"]);
+    expect(updatePos([1, 1, "W"], "R")).to.eql([1, 1, "N"]);
+  });
+  it("updates bearing correctly when given single instruction to turn left", () => {
+    expect(updatePos([1, 1, "N"], "L")).to.eql([1, 1, "W"]);
+    expect(updatePos([1, 1, "E"], "L")).to.eql([1, 1, "N"]);
+    expect(updatePos([1, 1, "S"], "L")).to.eql([1, 1, "E"]);
+    expect(updatePos([1, 1, "W"], "L")).to.eql([1, 1, "S"]);
+  });
+  it("updates position correctly when given single instruction to turn move forwards", () => {
+    expect(updatePos([1, 1, "N"], "M")).to.eql([1, 2, "N"]);
+    expect(updatePos([1, 1, "E"], "M")).to.eql([2, 1, "E"]);
+    expect(updatePos([1, 1, "S"], "M")).to.eql([1, 0, "S"]);
+    expect(updatePos([1, 1, "W"], "M")).to.eql([0, 1, "W"]);
+  });
 });
